8c32a1620bd58780add637fac1f556c91f19eeae
tsan0409 <tsan0409@gmail.com>
2022-11-23 02:38:04 +0900

    初コミット

---
+++ diary/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from.import views
+
+
+# app_name 他のアプリとの衝突を避けるため
+app_name = 'diary'
+# path(:https//<ホスト名>, IndexView, ルーティング処理の識別)
+# 第一引数とパターンがマッチングすれば、第二引数の処理を行う。第3引数のnameはルーティングの名前でURLを逆引きする時に使う
+#アプリケーション側のパターンマッチングは管理サーバーのパターンにプラスで加えるもの。
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+]
